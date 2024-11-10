bf6672b9c8e6dfe17a737688dda802d3f19995ba
xbalagex <gotthard.balazs@gmail.com>
2024-11-10 19:00:44 +0100

    elso_commit

--- packages/ui/src/views/chatflows/EmbedChat.jsx
+++ packages/ui/src/views/chatflows/EmbedChat.jsx
@@ -180,8 +180,8 @@ const chatwindowConfig = (isReact = false) => {
                     footer: {
                         textColor: '#303235',
                         text: 'Powered by',
-                        company: 'Flowise',
-                        companyLink: 'https://flowiseai.com',
+                        company: 'PHH',
+                        companyLink: 'https://proherohub.com',
                     }
                 }`
         : `chatWindow: {
